76a7e42299519e02424e1e85b01daaad083274b5
Konan <kixeyems0@gmail.com>
2024-07-09 16:24:21 +0100

    jee

--- dist/src/middleware/joi.js
+++ dist/src/middleware/joi.js
@@ -8,16 +8,20 @@ exports.createOrgSchema = exports.userLoginSchema = exports.userRegisterSchema =
 const joi_1 = __importDefault(require("joi"));
 exports.userRegisterSchema = joi_1.default.object({
     firstName: joi_1.default.string().required().messages({
+        "any.invalid": "Name cannot be null",
         "string.empty": "First name is required",
     }),
     lastName: joi_1.default.string().required().messages({
+        "any.invalid": "Name cannot be null",
         "string.empty": "Last name is required",
     }),
     email: joi_1.default.string().email().required().messages({
+        "any.invalid": "Name cannot be null",
         "string.email": "Invalid email address",
         "string.empty": "Email is required",
     }),
     password: joi_1.default.string().min(6).required().messages({
+        "any.invalid": "Name cannot be null",
         "string.min": "Password must be at least 6 characters long",
         "string.empty": "Password is required",
     }),
@@ -32,17 +36,17 @@ exports.userLoginSchema = joi_1.default.object({
     email: joi_1.default.string().email().required().messages({
         "string.email": "Invalid email address",
         "string.empty": "Email is required",
+        "any.invalid": "Name cannot be null",
     }),
-    password: joi_1.default.string().min(6).required().messages({
+    password: joi_1.default.string().min(6).not(null).required().messages({
         "string.min": "Password must be at least 6 characters long",
         "string.empty": "Password is required",
     }),
 });
 exports.createOrgSchema = joi_1.default.object({
-    name: joi_1.default.string().required().messages({
+    name: joi_1.default.string().not(null).required().messages({
+        "any.invalid": "Name cannot be null",
         "string.empty": "Name is required",
     }),
-    description: joi_1.default.string().required().messages({
-        "string.empty": "Description is required",
-    }),
+    description: joi_1.default.string().allow("").default(""),
 });
--- src/middleware/joi.ts
+++ src/middleware/joi.ts
@@ -3,16 +3,20 @@ import Joi from "joi";
 
 export const userRegisterSchema = Joi.object({
   firstName: Joi.string().required().messages({
+    "any.invalid": "Name cannot be null",
     "string.empty": "First name is required",
   }),
   lastName: Joi.string().required().messages({
+    "any.invalid": "Name cannot be null",
     "string.empty": "Last name is required",
   }),
   email: Joi.string().email().required().messages({
+    "any.invalid": "Name cannot be null",
     "string.email": "Invalid email address",
     "string.empty": "Email is required",
   }),
   password: Joi.string().min(6).required().messages({
+    "any.invalid": "Name cannot be null",
     "string.min": "Password must be at least 6 characters long",
     "string.empty": "Password is required",
   }),
@@ -29,18 +33,18 @@ export const userLoginSchema = Joi.object({
   email: Joi.string().email().required().messages({
     "string.email": "Invalid email address",
     "string.empty": "Email is required",
+    "any.invalid": "Name cannot be null",
   }),
-  password: Joi.string().min(6).required().messages({
+  password: Joi.string().min(6).not(null).required().messages({
     "string.min": "Password must be at least 6 characters long",
     "string.empty": "Password is required",
   }),
 });
 
 export const createOrgSchema = Joi.object({
-  name: Joi.string().required().messages({
+  name: Joi.string().not(null).required().messages({
+    "any.invalid": "Name cannot be null",
     "string.empty": "Name is required",
   }),
-  description: Joi.string().required().messages({
-    "string.empty": "Description is required",
-  }),
+  description: Joi.string().allow("").default(""),
 });
